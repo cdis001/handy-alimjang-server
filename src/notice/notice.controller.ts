e4d13faa5d9992e33ed33e207402424ad0825e49
JeonJinWook <jeonjinwook@JeonJinWook-ui-MacBook-Pro.local>
2022-01-03 21:49:27 +0900

    add notice  - notice co, s, mo, entity 추가

---
+++ src/notice/notice.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+
+import { NoticeService } from './notice.service';
+
+@Controller('notice')
+export class NoticeController {
+  constructor(private noticeService: NoticeService) {}
+
+  @Get('addNotice')
+  async addNotice(@Req() req) {
+    const notice = req.body;
+  }
+}
---
+++ src/notice/notice.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Notice } from './notice.entity';
+
+@Injectable()
+export class NoticeService {
+  constructor(
+    @InjectRepository(Notice)
+    private noticeRepository: Repository<Notice>,
+  ) {}
+}
